fc472308e352b66dd296b369202e817b2a9314dc
foobert <0x002a@gmail.com>
2018-03-25 12:05:19 +0200

    Automatically push to Docker hub

---
+++ bin/push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function main {
+  local branch=$(git rev-parse --abbrev-ref HEAD)
+  local tag=$(git show -s --format=%ct-%h HEAD)
+
+  if [ "${DOCKER_USERNAME}" != "" -a "${DOCKER_PASSWORD}" != "" ]
+  then
+    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+  fi
+
+  if [ "${branch}" = "master" ]
+  then
+    docker push foobert/jwtdummy:latest
+  else
+    tag="${tag}-${branch}"
+  fi
+
+  docker tag foobert/jwtdummy:latest "foobert/jwtdummy:${tag}"
+  docker push "foobert/jwtdummy:${tag}"
+}
+
+set -e
+main
